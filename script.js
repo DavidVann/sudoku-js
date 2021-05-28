28e171ae32e2517380fd5929a241e5c65042f7cf
DavidVann <32752619+DavidVann@users.noreply.github.com>
2021-05-28 13:25:21 -0400

    Working on puzzle reduction. Going to try to separate logic and HTML behavior.

--- script.js
+++ script.js
@@ -2,34 +2,41 @@ const container = document.getElementById("game-container");
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 
-const countOccurences = (array, value) => {
-    /**
-     * Returns number of occurrences of a value in array
-     */
-    return array.reduce((acc, currentVal) => acc + (currentVal == value), 0);
-}
+// const countOccurences = (array, value) => {
+//     /**
+//      * Returns number of occurrences of a value in array
+//      */
+//     return array.reduce((acc, currentVal) => acc + (currentVal == value), 0);
+// }
 
-const range = (start, stop) => {
-    /**
-     * Creates array of numbers: [start, stop)
-     */
-    if (stop === undefined) {
-        stop = start;
-        start = 0;
-    }
+// const range = (start, stop) => {
+//     /**
+//      * Creates array of numbers: [start, stop)
+//      */
+//     if (stop === undefined) {
+//         stop = start;
+//         start = 0;
+//     }
     
-    return [...Array(stop-start).keys()].map(e => e + start)
-}
+//     return [...Array(stop-start).keys()].map(e => e + start)
+// }
 
 const shuffle = (array) => {
-    // arr = [...array] // copy array
-    arr = array // in-place modification
-    n = arr.length
+    arr = [...array]; // copy array
+    // arr = array // in-place modification
+    n = arr.length;
     for(let i = n-1; i > 0; i--) {
         j = Math.floor(Math.random() * (i + 1));
         [arr[i], arr[j]] = [arr[j], arr[i]];
     }
     return arr;
+}
+
+const randInt = (max) => {
+    /**
+     * Generates a random number in range [0, max)
+     */
+    return Math.floor(Math.random() * (max))
 }
 
 class Cell {
@@ -39,9 +46,9 @@ class Cell {
         this.house = [];
         this.row = [];
         this.col = [];
-        this.element = document.createElement('div')
-        this.element.textContent = String(this.value)
-        this.element.classList.add('grid-cell')
+        this.element = document.createElement('div');
+        this.element.textContent = String(this.value);
+        this.element.classList.add('grid-cell');
     }
 
     update(value) {
@@ -82,7 +89,6 @@ class Cell {
             this.element.classList.remove('invalid')
         }
 
-
         return isValid;
     }
 }
@@ -94,12 +100,19 @@ class Grid {
         this.cols = 9;
         this.houses = 9;
         this.size = this.rows * this.cols;
-        this.grid = [];
+        this.cells = [];
+        this.solutions = 0;
+        // this.setup();
+    }
 
+    fillAssociateCells() {
+        /**
+         * Associates every cell in the grid with its row, column, and house for easy referencing.
+         */
         // Fill grid with Cells and populate DOM
         for (let i = 0; i < this.rows; i++) {
             let row = [];
-            this.grid.push(row);
+            this.cells.push(row);
 
             let rowDisplay = document.createElement('div')
             rowDisplay.classList.add('grid-row')
@@ -116,7 +129,7 @@ class Grid {
         for (let i = 0; i < this.rows; i++) {
             let row = [];
             for (let j = 0; j < this.cols ; j++) {
-                let cell = this.grid[i][j];
+                let cell = this.cells[i][j];
                 row.push(cell)
                 cell.row = row;
             }
@@ -126,15 +139,17 @@ class Grid {
         for (let j = 0; j < this.cols; j++) {
             let col = [];
             for (let i = 0; i < this.rows; i++) {
-                let cell = this.grid[i][j];
+                let cell = this.cells[i][j];
                 col.push(cell);
                 cell.col = col;
             }
         }
 
         // Associate each Cell with its house (3x3 square)
+        // Also, add a class to Cell's element for house coloring
         let rowStart = 0;
         let colStart = 0;
+        let coloredHouses = [1, 3, 5, 7];
         for (let k = 0; k < this.houses; k++) {
             let house = [];
             if (k % 3 === 0 && k > 0) {
@@ -143,7 +158,11 @@ class Grid {
             }
             for (let i = rowStart; i < rowStart + 3; i++) {
                 for (let j = colStart; j < colStart + 3; j++) {
-                    let cell = this.grid[i][j];
+                    let cell = this.cells[i][j];
+                    if (coloredHouses.includes(k)) {
+                        cell.element.classList.add('house-alt')
+                    }
+
                     cell.house = house;
                     house.push(cell);
                 }
@@ -154,11 +173,19 @@ class Grid {
         }
     }
 
-    validateGrid() {
+    validateSolution() {
+        for (let row of this.cells) {
+            for (let cell of row) {
+                cell.validate();
+            }
+        }
+    }
+
+    checkGenerated() {
         /**
-         * Validate generated Sudoku puzzle by checking for remaining 0s. Does not check for game solution.
+         * Validate generated Sudoku puzzle by checking for remaining 0s.
          */
-        for(let row of this.grid) {
+        for(let row of this.cells) {
             for(let cell of row) {
                 if (cell.value === 0) {
                     return false;
@@ -168,35 +195,36 @@ class Grid {
         return true;
     }
 
-    validateSolution() {
-        for (let row of this.grid) {
-            for (let cell of row) {
-                cell.validate();
-            }
-        }
-    }
-
-    generateGrid() {
+    solve(generating=false) {
         /**
-         * Generates a potential Sudoku puzzle
+         * Solves a sudoku puzzle using backtracking. Can also be used for puzzle generation.
          */
-        let row, col, cell;
+        let row, col, cell, candidates;
         for (let i = 0; i < this.size; i++) {
             row = Math.floor(i / 9);
             col = i % 9;
-            cell = this.grid[row][col];
+            cell = this.cells[row][col];
 
             // A cell with a value of 0 indicates that it has not yet been solved, so try to find a valid number for it.
             if (cell.value === 0) {
-                shuffle(nums); // shuffle valid numbers for randomness in puzzle generation
-                for(let num of nums) {
+                if (generating) {
+                    candidates = shuffle(nums); // shuffle valid numbers for randomness in puzzle generation
+                } else {
+                    candidates = nums;
+                }
+                for(let num of candidates) {
                     if (cell.validate(num)) {
                         cell.update(num);
-                        if (this.validateGrid()) {
-                            // If this chain of function calls has led to a solution, we're done; they'll all return true.
-                            return true;
+                        if (this.checkGenerated()) {
+                            if (!generating) {
+                                // If we're solving (removing hints), break if we find a solution
+                                this.solutions++;
+                                break
+                            } else {
+                                return true;
+                            }
                         }
-                        else if(this.generateGrid()) {
+                        else if(this.solve(generating)) {
                             // Recursively check all solutions branching from this one.
                             // A successful solution chain will result in an exit here.
                             return true;
@@ -214,69 +242,111 @@ class Grid {
         cell.update(0);
     }
 
+    removeHints(attempts) {
+        /**
+         * Starting from a solved grid, iteratively remove cells to create a playable board, checking that it's still solveable.
+         */
+        if (attempts === undefined) {
+            attempts = 1;
+        }
+        while (attempts > 0) {
+            // Pick a random non-empty cell
+            let row = randInt(9);
+            let col = randInt(9);
+            while (this.cells[row][col].value === 0) {
+                // Pick another cell if selected one is empty
+                row = randInt(9);
+                col = randInt(9);
+            }
 
-    solveGrid() {
+            let backup = this.cells[row][col].value
+            this.cells[row][col].update(0);
+            // console.log(backup)
+
+
+            this.solve();
+            if (this.solutions != 1) {
+                console.log(this.solutions);
+                this.cells[row][col].update(backup)
+                attempts -= 1;
+            }
+        }
+    }
+
+    copy() {
+        g = new Grid();
         
     }
 
-    randomize() {
-        for (let row of this.grid) {
-            for (let cell of row) {
-                cell.update(Math.floor(Math.random()*9))
-            }
-        }
+
+    setup() {
+        this.fillAssociateCells();
+        this.solve(true);
+        this.removeHints(5);
+        // this.validateSolution();
     }
 
-    pprint() {
-        let g = [];
-        for (let row of this.grid) {
-            let rowVals = [];
-            g.push(rowVals);
-            for (let cell of row) {
-                rowVals.push(cell.value);
+
+    // solve() {
+    //     let row, col, cell;
+    //     for (let i = 0; i < this.size; i++) {
+    //         row = Math.floor(i / 9);
+    //         col = i % 9;
+    //         cell = this.cells[row][col];
+
+    //         if (cell.value === 0) {
+    //             for (let num of range(1, 10)) {
+    //                 if (cell.validate(num)) {
+                        
+    //                 }
+    //             }
+    //         }
+
+    //     }
+    // }
+
+}
+
+class HTMLManager {
+    constructor(grid) {
+        this.g = grid;
+    }
+
+    updateState() {
+        for (row of this.g.cells) {
+            for (cell of row) {
+                
             }
         }
-        console.log(g);
     }
 
 }
 
+class GameManager {
+    constructor() {
+        this.g = new Grid();
+
+        this.active = null;
+        this.prevActive = null;
+        this.setup();
+    }
+
+    setup() {
+        this.g.setup();
+    }
 
 
-g = new Grid()
-g.generateGrid();
-g.validateSolution();
+    
+}
+
+(runApplication = () => {
+    gm = new GameManager();
+})();
+
+// g = new Grid();
+// g.generateGrid();
+// g.validateSolution();
 // g.pprint();
 
-// g.randomize()
-// g.display()
-
 // Create 9x9 grid of 0s
-let grid = [...Array(9)].map(() => Array(9).fill(0));
-let testGrid = [...Array(9)].map(() => Array(9).fill(new Cell(0)))
-
-// const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-// for(let i=0; i < 81; i++) {
-//     row = Math.floor(i / 9);
-//     col = i % 9;
-//     if (grid[row][col] === 0) {
-//         shuffle(nums);
-//         for (num of nums) {
-//             let gridRow = grid[row];
-//             let gridCol = [];
-//             for (let colRow=0; colRow < 9; colRow++) {
-//                 gridCol.push(grid[colRow][col]);
-//             }
-//             let house = [];
-//             if (row < 3) {
-//                 if (col < 3){
-//                     house = 1;
-//                 }
-//             }
-//             if (!gridRow.includes(num) && !gridCol.includes(num)){
-
-//             }
-//         }
-//     }
-
-// }
+// let grid = [...Array(9)].map(() => Array(9).fill(0));
